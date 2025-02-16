6a9bfcad7e83d02bb4939c18327f26a73d58212f
rahul <65274730+rsyeul@users.noreply.github.com>
2025-02-16 14:12:19 +0530

    remove unused variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './component/Hearder';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter,} from 'react-router-dom';
 import Footer from './component/Footer';
-import { Home } from 'lucide-react';
 import ProductsList from './component/Products';
 
 function App() {
   return (
-  // <BrowserRouter>
-  //     <Routes>
-  //       <Route path="/" element={<Header />} />
-  //     </Routes>
-  //   </BrowserRouter>
-
-  <BrowserRouter>
-    
-    <div className="min-h-screen flex flex-col">
-    <Header />
-
-    <ProductsList></ProductsList>
-
-    <main className="flex-grow">
-      <Routes>
-
-      </Routes>
-    </main>
-    <Footer />
-  </div>
-</BrowserRouter>
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <ProductsList></ProductsList>
+        <Footer />
+      </div>
+  </BrowserRouter>
 
   );
 }
